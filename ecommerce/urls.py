aad306ef901fb4dc21be928fb6b4524f392253ad
Khalid Bashier <khalidbashier@khalids-mbp.lan>
2019-12-18 12:49:30 +0000

    first commit

---
+++ ecommerce/urls.py
@@ -0,0 +1,32 @@
+"""ecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from .views import (
+	home_page,
+	exclusive_page,
+    tracksuits_page,
+    contact_page,
+	)
+urlpatterns = [
+	path('', home_page),
+    path('contact/', contact_page),
+	path('exclusive/', exclusive_page),
+    path('tracksuits/', tracksuits_page),
+    path('admin/', admin.site.urls),
+  
+]
---
+++ ecommerce/views.py
@@ -0,0 +1,69 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+from .forms import ContactForm
+import random
+
+def home_page(request):
+	context ={
+	"title": "THE OFFICIAL HUMBLE HOTHEADS WEBSITE",
+	"content": "Welcome to the home page",
+	"premium_content": "YEEAAAHH",
+	'n': random.random()
+
+		}
+	return render(request, 'home_page.html' , context)
+
+
+
+def exclusive_page(request):
+	context = {
+	"title": "Exclusive Items",
+	"content": "Check out the latest exclusive items",
+	}
+	return render(request, 'home_page.html' , context)
+    
+
+
+
+def tracksuits_page(request):
+	context = {
+	"title": "Tracksuits",
+	"content": "Tracksuits",
+	}
+
+	return render(request, 'home_page.html', context)
+
+def login_page(request):
+	return render(request, "auth/login.html", {})
+
+def register_page(request):
+	return render(request, "auth/login.html", {})
+
+def contact_page(request):
+	contact_form = ContactForm(request.POST or None)
+	context = {
+	"title": "Welcome to our contact page",
+	"content": "Please fill in your details to get our latest drops",
+	"form": contact_form
+	}
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data )
+	#if request.method == "POST" :
+		# print (request.POST) 
+		# print (request.POST.get('fullname'))
+		# print (request.POST.get('email'))
+		# print (request.POST.get('content'))
+	return render(request, 'contact/view.html', context)
+
+
+
+
+
+
+    
+
+
+
+
+
